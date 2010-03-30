6c1969959d8e0961fa5075dac2f76e60b7f2c5fe
Chris Klimas <klimas@gmail.com>
2010-03-30 02:30:22 +0000

    Fixed API call mismatches

--- lib/tiddlywiki.py
+++ lib/tiddlywiki.py
@@ -12,9 +12,7 @@
 # that translate between Twee and TiddlyWiki output seamlessly.
 #
 
-import re
-import datetime
-import time
+import re, datetime, time, os, sys
 import PyRSS2Gen as rss
 
 #
@@ -22,45 +20,80 @@ import PyRSS2Gen as rss
 #
 
 class TiddlyWiki:
-	"represents an entire TiddlyWiki"
+	"""An entire TiddlyWiki."""
 	
 	def __init__ (self, author = 'twee'):
-		"constructor; optionally pass an author name"
+		"""Optionally pass an author name."""
 		self.author = author
 		self.tiddlers = {}
 
-
-	def try_getting (self, names, default = ''):
-		"tries retrieving the text of several tiddlers by name; returns default if none exist"
+	def tryGetting (self, names, default = ''):
+		"""Tries retrieving the text of several tiddlers by name; returns default if none exist."""
 		for name in names:
 			if name in self.tiddlers:
 				return self.tiddlers[name].text
 				
 		return default
 		
-		
-	def to_twee (self):
-		"returns Twee source code for this TiddlyWiki"
+	def toTwee (self, order = None):
+		"""Returns Twee source code for this TiddlyWiki."""
+		if not order: order = self.tiddlers.keys()		
 		output = ''
 		
-		for i in self.tiddlers:
-			output += self.tiddlers[i].to_twee()
+		for i in order:
+			output += self.tiddlers[i].toTwee()
 		
 		return output
 		
-	
-	def to_html (self):
-		"returns HTML code for this TiddlyWiki"
+	def toHtml (self, app, target = None, order = None):
+		"""Returns HTML code for this TiddlyWiki. If target is passed, adds a header."""
+		if not order: order = self.tiddlers.keys()
 		output = ''
 		
-		for i in self.tiddlers:
-			output += self.tiddlers[i].to_html(self.author)
+		if (target):
+			header = open(app.getPath() + os.sep + 'targets' + os.sep + target + os.sep + 'header.html')
+			output = header.read()
+			header.close()
+		
+		for i in order:
+			output += self.tiddlers[i].toHtml(self.author)
+		
+		if (target):
+			output += '</div></body></html>'
+		
+		return output
+	
+	def toRtf (self, order = None):
+		"""Returns RTF source code for this TiddlyWiki."""
+		if not order: order = self.tiddlers.keys()
+		
+		# preamble
+		
+ 		output = r'{\rtf1\ansi\ansicpg1251' + '\n'
+		output += r'{\fonttbl\f0\fswiss\fcharset0 Arial;}' + '\n'
+		output += r'{\colortbl;\red128\green128\blue128;}' + '\n'
+		output += r'\margl1440\margr1440\vieww9000\viewh8400\viewkind0' + '\n'
+		output += r'\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx792' + '\n'
+		output += r'\tx8640\ql\qnatural\pardirnatural\pgnx720\pgny720' + '\n'
+		
+		# content
+		
+		for i in order:
+			text = self.tiddlers[i].text
+			text = re.sub(r'\n', '\\\n', text) # newlines
+			text = re.sub(r'\[\[(.*?)\]\]', r'\ul \1\ulnone ', text) # links
+			text = re.sub(r'\/\/(.*?)\/\/', r'\i \1\i0 ', text) # italics
+			text = re.sub(r'(\<\<.*?\>\>)', r'\cf1 \i \1\i0 \cf0', text) # macros
+
+			output += r'\fs24\b1' + self.tiddlers[i].title + r'\b0\fs20' + '\\\n'
+			output += text + '\\\n\\\n'
+		
+		output += '}'
 			
 		return output
 		
-		
-	def to_rss (self, num_items = 5):
-		"returns an RSS2 object of recently changed tiddlers"
+	def toRss (self, num_items = 5):
+		"""Returns an RSS2 object of recently changed tiddlers."""
 		url = self.try_getting(['StoryUrl', 'SiteUrl'])
 		title = self.try_getting(['StoryTitle', 'SiteTitle'], 'Untitled Story')
 		subtitle = self.try_getting(['StorySubtitle', 'SiteSubtitle'])
@@ -75,7 +108,7 @@ class TiddlyWiki:
 		rss_items = []
 		
 		for i in sorted_keys[:num_items]:
-			rss_items.append(self.tiddlers[i].to_rss())
+			rss_items.append(self.tiddlers[i].toRss())
 				
 		return rss.RSS2(
 			title = title,
@@ -85,29 +118,26 @@ class TiddlyWiki:
 			items = rss_items
 			)
 		
-	
-	def add_twee (self, source):
-		"converts Twee source code to tiddlers in this TiddlyWiki"
+	def addTwee (self, source):
+		"""Adds Twee source code to this TiddlyWiki."""
 		source = source.replace("\r\n", "\n")
 		tiddlers = source.split('\n::')
 		
 		for i in tiddlers:
-			self.add_tiddler(Tiddler('::' + i))
+			self.addTiddler(Tiddler('::' + i))
 			
-	
-	def add_html (self, source):
-		"converts HTML source code to tiddlers in this TiddlyWiki"
+	def addHtml (self, source):
+		"""Adds HTML source code to this TiddlyWiki."""
 		divs_re = re.compile(r'<div id="storeArea">(.*)</div>\s*</html>',
 												 re.DOTALL)
 		divs = divs_re.search(source)
 
 		if divs:
 			for div in divs.group(1).split('<div'):
-				self.add_tiddler(Tiddler('<div' + div, 'html'))
-		
+				self.addTiddler(Tiddler('<div' + div, 'html'))
 
-	def add_tiddler (self, tiddler):
-		"adds a tiddler to this TiddlyWiki"
+	def addTiddler (self, tiddler):
+		"""Adds a Tiddler object to this TiddlyWiki."""
 		
 		if tiddler.title in self.tiddlers:
 			if (tiddler == self.tiddlers[tiddler.title]) and \
@@ -115,32 +145,30 @@ class TiddlyWiki:
 				self.tiddlers[tiddler.title] = tiddler
 		else:
 			self.tiddlers[tiddler.title] = tiddler
-
 		
 #
 # Tiddler class
 #
 
-		
 class Tiddler:
-	"represents a single tiddler in a TiddlyWiki"
+	"""A single tiddler in a TiddlyWiki."""
 	
 	def __init__ (self, source, type = 'twee'):
-		"constructor; pass source code, and optionally 'twee' or 'html'"
+		"""Pass source code, and optionally 'twee' or 'html'"""
 		if type == 'twee':
-			self.init_twee(source)
+			self.initTwee(source)
 		else:
-			self.init_html(source)
+			self.initHtml(source)
 
+	def __repr__ (self):
+		return "<Tiddler '" + self.title + "'>"
 
 	def __cmp__ (self, other):
-		"compares a Tiddler to another"
+		"""Compares a Tiddler to another."""
 		return self.text == other.text
-		
-
-			
-	def init_twee (self, source):
-		"initializes a Tiddler from Twee source code"
+	
+	def initTwee (self, source):
+		"""Initializes a Tiddler from Twee source code."""
 	
 		# we were just born
 		
@@ -173,8 +201,8 @@ class Tiddler:
 		self.text = self.text.strip()
 		
 		
-	def init_html (self, source):
-		"initializes a Tiddler from HTML source code"
+	def initHtml (self, source):
+		"""Initializes a Tiddler from HTML source code."""
 	
 		# title
 		
@@ -217,8 +245,8 @@ class Tiddler:
 			self.text = decode_text(text.group(1))
 				
 		
-	def to_html (self, author = 'twee'):
-		"returns an HTML representation of this tiddler"
+	def toHtml (self, author = 'twee'):
+		"""Returns an HTML representation of this tiddler."""
 			
 		now = time.localtime()
 		output = '<div tiddler="' + self.title + '" tags="'
@@ -235,22 +263,23 @@ class Tiddler:
 		return output
 		
 		
-	def to_twee (self):
-		"returns a Twee representation of this tiddler"
+	def toTwee (self):
+		"""Returns a Twee representation of this tiddler."""
 		output = ':: ' + self.title
 		
 		if len(self.tags) > 0:
 			output += ' ['
 			for tag in self.tags:
 				output += tag + ' '
-			output = output.trim()
+			output = output.strip()
+			output += ']'
 			
 		output += "\n" + self.text + "\n\n\n"
 		return output
 		
 		
-	def to_rss (self, author = 'twee'):
-		"returns an RSS representation of this tiddler"
+	def toRss (self, author = 'twee'):
+		"""Returns an RSS representation of this tiddler."""
 		return rss.RSSItem(
 			title = self.title,
 			link = '',
@@ -258,16 +287,60 @@ class Tiddler:
 			pubDate = datetime.datetime.now()
 		)
 
+	def links (self, includeExternal = False):
+		"""
+		Returns a list of all passages linked to by this one. By default,
+		only returns internal links, but you can override it with the includeExternal
+		parameter.
+		"""
+
+		# regular hyperlinks
 		
+		links = re.findall(r'\[\[(.+?)\]\]', self.text)
+	
+		# check for [[title|target]] formats
+
+		def filterPrettyLinks (text):
+			if '|' in text: return re.sub('.*\|', '', text)
+			else: return text
+			
+		# remove external links
+		
+		def isInternalLink (text):
+			return not re.match('http://', text)
+		
+		links = map(filterPrettyLinks, links)
+		if not includeExternal: links = filter(isInternalLink, links)
+
+		# <<display ''>>
+		
+		displays = re.findall(r'\<\<display\s+[\'"](.+?)[\'"]\s?\>\>', self.text, re.IGNORECASE)
+		
+		# <<choice ''>>
+		
+		choices = re.findall(r'\<\<choice\s+[\'"](.+?)[\'"]\s?\>\>', self.text, re.IGNORECASE)
+
+		# <<actions ''>>
+		
+		actions = list()
+		actionBlocks = re.findall(r'\<\<actions\s+(.*?)\s?\>\>', self.text, re.IGNORECASE)
+		for block in actionBlocks:
+			actions = actions + re.findall(r'[\'"](.*?)[\'"]', block)
+		
+		# remove duplicates by converting to a set
+		
+		return list(set(links + displays + choices + actions))
+
 #
 # Helper functions
 #
 
 
 def encode_text (text):
+	"""Encodes a string for use in HTML output."""
 	output = text
 	output = output.replace('\\', '\s')
-	output = output.replace('\n', '\\n')
+	output = re.sub(r'\r?\n', r'\\n', output)
 	output = output.replace('<', '&lt;')
 	output = output.replace('>', '&gt;')
 	output = output.replace('"', '&quot;')
@@ -276,6 +349,7 @@ def encode_text (text):
 
 	
 def decode_text (text):
+	"""Decodes a string from HTML."""
 	output = text
 	output = output.replace('\\n', '\n')
 	output = output.replace('\s', '\\')
@@ -287,8 +361,10 @@ def decode_text (text):
 	
 	
 def encode_date (date):
+	"""Encodes a datetime in TiddlyWiki format."""
 	return time.strftime('%Y%m%d%H%M', date)
 	
 
 def decode_date (date):
-	return time.strptime(date, '%Y%m%d%H%M')	
+	"""Decodes a datetime from TiddlyWiki format."""
+	return time.strptime(date, '%Y%m%d%H%M')
